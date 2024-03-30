4ebb17938d42b42e1723e6f8ab83c0241d31fea3
YeongJun <eric040928@naver.com>
2024-03-30 21:52:17 +0900

    first commit

---
+++ app/src/main/java/kr/co/lion/groupgittest/MainActivity.kt
@@ -0,0 +1,14 @@
+package kr.co.lion.groupgittest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.d("test1234", "그룹 깃헙")
+    }
+}
